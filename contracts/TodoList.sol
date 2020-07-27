a3a9111f8930ffd9db6b51e1072b7f7c3dd83213
sachitha2 <shpsachitha@gmail.com>
2020-07-28 00:15:21 +0530

    create tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -13,6 +13,12 @@ contract TodoList{
 
     mapping(uint => Task) public tasks;
 
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
     constructor() public{
         createTask("chatson.com");
     }
@@ -20,5 +26,6 @@ contract TodoList{
     function createTask(string memory _content) public{
         taskCount++;
         tasks[taskCount] = Task(taskCount,_content,false);
+        emit TaskCreated(taskCount,_content,false);
     }
 }
--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -14,11 +14,21 @@ contract('TodoList', (accounts) => {
         assert.notEqual(address, undefined)
       })
 
+      it('lists tasks', async () => {
+        const taskCount = await this.todoList.taskCount()
+        const task = await this.todoList.tasks(taskCount)
+        assert.equal(task.id.toNumber(), taskCount.toNumber())
+        assert.equal(task.content, 'chatson.com')
+        assert.equal(task.completed, false)
+        assert.equal(taskCount.toNumber(), 1)
+      })
+
 
       it('creates tasks', async () => {
         const result = await this.todoList.createTask('A new task')
         const taskCount = await this.todoList.taskCount()
         assert.equal(taskCount, 2)
+        console.log(result)
         const event = result.logs[0].args
         assert.equal(event.id.toNumber(), 2)
         assert.equal(event.content, 'A new task')
